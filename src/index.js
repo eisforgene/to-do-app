0148881d6f32209ebff2255e022257077b8dcbf6
eisforgene <82473195+eisforgene@users.noreply.github.com>
2021-12-08 23:42:05 -0800

    starter code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+const { ApolloServer, gql } = require('apollo-server'); 
 
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const typeDefs = gql`
+  type Query {
+    books: [Book]
+  }
+
+  type Book {
+    title: String
+    author: String
+  }  
+ 
+`;
+
+// Resolvers = how we get data from specific fields
+// They have same structure as typeDefs
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
